1409f5299bf811c1bf294084467ce7719ae3eabc
lee <jcsoft@aliyun.com>
2017-12-05 10:41:19 +0800

    fixed find php.ini error

--- scripts/start.sh
+++ scripts/start.sh
@@ -45,7 +45,7 @@ fi
 if [[ "$HIDE_NGINX_HEADERS" == "0" ]] ; then
  sed -i "s/server_tokens off;/server_tokens on;/g" /etc/nginx/nginx.conf
 else
- sed -i "s/expose_php = On/expose_php = Off/g" /etc/php7/php.ini
+ #sed -i "s/expose_php = On/expose_php = Off/g" /etc/php7/php.ini
 fi
 
 # Pass real-ip to logs when behind ELB, etc
